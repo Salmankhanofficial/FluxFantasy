d3e0e0cd1ae54752633ebc4dc66f9b521c1a9fb3
afaq000 <afaqhussain098@gmail.com>
2024-04-30 03:25:36 -0700

    afaq-10

--- src/components/Faction.tsx
+++ src/components/Faction.tsx
@@ -1,32 +1,27 @@
 import React, { useState } from 'react';
-
 interface CheckboxItem {
   id: number;
   label: string;
 }
-
 interface Props {
   className?: string;
   checkboxes: CheckboxItem[];
   onCheckboxChange: (id: number) => void;
 }
-
 const Faction: React.FC<Props> = ({ className, checkboxes, onCheckboxChange }) => {
   const [selectedCheckbox, setSelectedCheckbox] = useState<number | null>(null);
-
   const handleCheckboxChange = (id: number) => {
     setSelectedCheckbox(id);
     onCheckboxChange(id);
   };
-
   return (
-    <div className={` w-full flex flex-col bg-[#EDEDED]`} >
+    <div className={` w-full flex flex-col bg-black`} >
       <div className={`${className} w-full `}>
         {checkboxes.map((checkbox) => (
-          <div key={checkbox.id} className="flex gap-2 py-1">
-            <input 
-              type="checkbox" 
-              id={`checkbox-${checkbox.id}`} 
+          <div key={checkbox.id} className="flex gap-2 py-1 text-gray-200">
+            <input
+              type="checkbox"
+              id={`checkbox-${checkbox.id}`}
               checked={selectedCheckbox === checkbox.id}
               onChange={() => handleCheckboxChange(checkbox.id)}
             />
@@ -37,5 +32,4 @@ const Faction: React.FC<Props> = ({ className, checkboxes, onCheckboxChange }) =
     </div>
   );
 };
-
 export default Faction;
