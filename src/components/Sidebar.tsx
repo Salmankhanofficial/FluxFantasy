06cf8dc2692ac880bb9deeb3a980cefd8ab299c7
Salmankhanofficial <salmanpak09@gmail.com>
2024-06-14 17:06:43 +0500

    asd

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,7 +13,7 @@ export interface props {
   notificationLength: number;
   onClick?: () => void;
 }
-const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
+const Sidebar = ({ mainClassName, notificationLength, onClick }: props) => {
   const router = useRouter();
 
   const data = [
@@ -27,21 +27,21 @@ const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
     { title: "Discord" },
   ];
   return (
-    
     <div
       className={` sm:pr-2   flex flex-col  relative bg-black ${mainClassName}`}
     >
-            <div className="bg-[#100D1C] h-24 w-[260px] z-50 absolute -top-20   ">
+      <div className="bg-[#100D1C] h-24 w-[260px] z-50 absolute -top-20   "></div>
 
-</div>
-
-            <div className="bg-black h-24 w-36 rotate-[55deg] absolute top-8 -right-20 ">
-
-</div>
+      <div className="bg-black h-24 w-36 rotate-[55deg] absolute top-8 -right-20 "></div>
 
       {/* Logo div start from here*/}
-      <div onClick={onClick} className="text-white text-4xl flex justify-end pr-3 sm:hidden">x</div>
-   
+      <div
+        onClick={onClick}
+        className="text-white text-4xl flex justify-end pr-3 sm:hidden"
+      >
+        x
+      </div>
+
       <div className={`text-white sticky flex justify-center pt-4`}>
         <Image
           width={250}
@@ -54,7 +54,7 @@ const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
       <div className="px-4 flex justify-between items-center">
         <div className="text-yellow-500 text-xl">Ella</div>
         <div className="flex gap-3 cursor-pointer">
-          <RiAccountCircleFill  className="text-white" />
+          <RiAccountCircleFill className="text-white" />
           <MdOutlineMail className="text-white" />
           <IoIosNotificationsOutline
             className={`${
@@ -64,13 +64,10 @@ const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
         </div>
       </div>
 
-      <div
-     
-        className="items-center sm:items-start flex flex-col  px-2 font-kallisto text-[#ABABAB]"
-      >
+      <div className="items-center sm:items-start flex flex-col  px-2 font-kallisto text-[#ABABAB]">
         <div className="flex flex-col ">
           <div className="px-2 mt-4">
-            <ul className="px-2 mt-4" >
+            <ul className="px-2 mt-4">
               {/* Mapping over the array to generate li elements */}
               {data.map((item, index) => (
                 <li key={index}>
@@ -80,7 +77,9 @@ const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
                   >
                     <div
                       onClick={() => router.push("/")}
-                      className={` cursor-pointer hover:text-red-300 ${index < 1 ? "text-yellow-400" : "text-[#ABABAB]"}`}
+                      className={` cursor-pointer hover:text-red-300 ${
+                        index < 1 ? "text-yellow-400" : "text-[#ABABAB]"
+                      }`}
                     >
                       {item.title}
                     </div>
@@ -90,7 +89,12 @@ const Sidebar = ({ mainClassName, notificationLength,onClick }: props) => {
             </ul>
 
             <div className="bg-[#1A1A1A] w-[200px] h-[600px] mb-40 mt-10  flex justify-center items-center">
-              <p className="text-[#313030] font-semibold" style={{ fontFamily: 'Kallisto'   }}>300 X 600 AD</p>
+              <p
+                className="text-[#313030] font-semibold"
+                style={{ fontFamily: "Kallisto" }}
+              >
+                300 X 600 AD
+              </p>
             </div>
           </div>
         </div>
